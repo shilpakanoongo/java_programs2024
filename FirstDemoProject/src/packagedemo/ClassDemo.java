f8bef3d73850ef7a6d315df77c0f4e468f8e826f
Shilpa Kanoongo <kanoongo.shilpa@gmail.com>
2024-10-17 11:19:14 -0400

    First java program in github

---
+++ FirstDemoProject/src/packagedemo/ClassDemo.java
@@ -0,0 +1,10 @@
+package packagedemo;
+
+public class ClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("This is my first java Program");
+	}
+
+}
