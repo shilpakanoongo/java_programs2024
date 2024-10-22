50b4611241090be92b146db6b68f14ed5f6c4787
Shilpa Kanoongo <kanoongo.shilpa@gmail.com>
2024-10-22 10:52:24 -0400

    by shilpa

---
+++ FirstDemoProject/src/AdditionDemo/Add.java
@@ -0,0 +1,20 @@
+package AdditionDemo;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+       System.out.println("enter first digit: ");
+       int a = sc.nextInt();
+       System.out.println("enter second digit: ");
+       int b = sc.nextInt();
+       int c = a+b;
+       System.out.println("addtion of two no. is : "+c);
+       
+       
+	}
+
+}
